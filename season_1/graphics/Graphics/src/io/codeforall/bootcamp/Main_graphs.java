f22c6ac74bcba2efa4ecd01e0c3cfcae2fe55ae5
josepinto70 <j2mpinto@hotmail.com>
2024-10-08 17:55:03 +0100

    20241008

---
+++ season_1/graphics/Graphics/src/io/codeforall/bootcamp/Main_graphs.java
@@ -0,0 +1,16 @@
+package io.codeforall.bootcamp;
+
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
+public class Main_graphs {
+
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle(100,100,44,44);
+        rectangle.setColor(Color.RED);
+        rectangle.fill();
+    }
+
+
+}
